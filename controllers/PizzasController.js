3ccf69ef856b4d4f0de501567ec44ba598a95d72
Carlos Machado <yeckapum@gmail.com>
2021-12-03 11:49:52 -0300

    Adicionado pizzas no controller

--- controllers/PizzasController.js
+++ controllers/PizzasController.js
@@ -2,7 +2,7 @@ const pizzas = require('../database/Pizzas.json');
 
 const controller = {
     listar: (req,res) => {
-        res.send('Listando as Pizzas...')
+        res.send(pizzas)
     }
 }
 
